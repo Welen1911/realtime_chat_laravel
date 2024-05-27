f51ba4ee0dd148a11b006baf652599cd4f96b2a9
Welen1911 <welenalmeida2210@gmail.com>
2024-05-26 21:24:23 -0300

    Feito: mudancas finais parcial

--- app/Http/Controllers/web/PageController.php
+++ app/Http/Controllers/web/PageController.php
@@ -22,7 +22,7 @@ class PageController extends Controller
 
     public function dashboard()
     {
-        return Inertia::render('Dashboard');
+        return redirect()->route('chart');
     }
 
     public function chart()
